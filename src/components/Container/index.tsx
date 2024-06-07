ef486f739ea854da315903f1982a0adbd105fc5c
Vladyslav <vlad.drobot32@gmail.com>
2024-06-07 22:50:41 +0300

    feat: add head panel

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,18 +1,22 @@
 import { CategoryContainer } from "../Category/CategoryContainer";
 import { useMouseMove } from "../../hooks/useMouseMove";
+import { HeadPanel } from "../HeadPanel";
 
 export const Container = () => {
   const { postition, handleStartMove, handleMoving, handleStopMove } =
     useMouseMove();
 
   return (
-    <div
-      className="container"
-      onMouseLeave={handleStopMove}
-      onMouseUp={handleStopMove}
-      onMouseMove={handleMoving}
-    >
-      <CategoryContainer onMouseDown={handleStartMove} style={postition} />
+    <div className="container">
+      <HeadPanel />
+      <div
+        className="category-field"
+        onMouseLeave={handleStopMove}
+        onMouseUp={handleStopMove}
+        onMouseMove={handleMoving}
+      >
+        <CategoryContainer onMouseDown={handleStartMove} style={postition} />
+      </div>
     </div>
   );
 };
