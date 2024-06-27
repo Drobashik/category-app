0fed23dfbbae8c914fecc4d23eae236ca452ab3c
Vladyslav <vlad.drobot32@gmail.com>
2024-06-27 23:25:04 +0300

    refactor: code improving

--- src/hooks/useMouseMove.ts
+++ src/hooks/useMouseMove.ts
@@ -14,11 +14,15 @@ export const useMouseMove = () => {
     setPosition({ x, y });
   };
 
-  const moveToScreenCenter = () => {
-    setPosition({ x: window.innerWidth / 2, y: window.innerHeight / 2 });
+  const moveToScreenCenter = (xModifier = 0, yModifier = 0) => {
+    setPosition({
+      x: window.innerWidth / 2 + xModifier,
+      y: window.innerHeight / 2 + yModifier,
+    });
   };
 
   const startMove = ({ clientX, clientY }: MouseEvent) => {
+    console.log(position);
     relativePosition.current = {
       x: clientX - position.x,
       y: clientY - position.y,
@@ -32,7 +36,7 @@ export const useMouseMove = () => {
 
     changePosition(
       clientX - relativePosition.current.x,
-      clientY - relativePosition.current.y,
+      clientY - relativePosition.current.y
     );
   };
 
